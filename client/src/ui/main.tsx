7fa7f4c7b5904d4aa5d47244569b2c9c4b670fdb
devedsonalves <devedsonalves@gmail.com>
2025-03-26 12:16:17 -0300

    chore: add lib "Amplify UI"

--- client/src/ui/main.tsx
+++ client/src/ui/main.tsx
@@ -1,5 +1,10 @@
 import './index.css'
 
+import '@aws-amplify/ui-react/styles.css'
+import '@aws-amplify/ui-react/styles/reset.layer.css'
+import '@aws-amplify/ui-react/styles/base.layer.css'
+import '@aws-amplify/ui-react/styles/button.layer.css'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app'
