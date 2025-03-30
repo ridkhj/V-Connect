e566a7d1e820248b9e0c96e60c0978a8e3e17588
devedsonalves <devedsonalves@gmail.com>
2025-03-30 15:17:26 -0300

    feature: create sidebar component

---
+++ client/src/ui/components/sidebar.tsx
@@ -0,0 +1,18 @@
+import FileUploader from "./file-uploader"
+
+export default function Sidebar() {
+    return (
+        <div className="relative w-1/3 h-screen flex flex-col items-center justify-start border-r-2 border-[#DDD] p-6">
+            <div className="flex justify-start items-center gap-1">
+                <img src="/logo.png" alt="Logo" className="w-1/7 h-auto" />
+                <p className="text-2xl font-semibold">Connect</p>
+            </div>
+            <div className="mt-5 w-full">
+                <FileUploader />
+            </div>
+            <div className="absolute bottom-0 left-0 w-full border-t-2 border-#ddd p-2 flex justify-center items-center">
+                <p className="text-xs font-medium">© 20025 V Connect | Todos os direitos reservados.</p>
+            </div>
+        </div>
+    )
+}
