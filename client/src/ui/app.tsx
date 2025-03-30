c0fcf7df8f45977901be99a87cc01d1bba8a0d78
devedsonalves <devedsonalves@gmail.com>
2025-03-30 15:18:43 -0300

    feature: create homepage

--- client/src/ui/app.tsx
+++ client/src/ui/app.tsx
@@ -1,21 +1,15 @@
+
+import Sidebar from "./components/sidebar";
 import OptionsComponent from "./components/options";
+
 
 export default function App() {
   return (
-    <>
-      <p
-        style={{
-          width: "100vw",
-          flex: 1,
-          display: "flex",
-          flexDirection: "column",
-          gap: "2rem",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+    <div className="w-screen h-screen flex overflow-hidden">
+      <Sidebar />
+      <div className="flex justify-center items-center w-full h-full bg-[#F1F1F1]">
         <OptionsComponent />
-      </p>
-    </>
+      </div>
+    </div>
   );
 }
