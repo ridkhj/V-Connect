967266e115d9d13bc8409d86de25a04c0dcf2dc8
devedsonalves <devedsonalves@gmail.com>
2025-03-30 15:20:47 -0300

    chore: clear imports

--- client/src/ui/main.tsx
+++ client/src/ui/main.tsx
@@ -1,10 +1,5 @@
 import './index.css'
 
-import '@aws-amplify/ui-react/styles.css'
-import '@aws-amplify/ui-react/styles/reset.layer.css'
-import '@aws-amplify/ui-react/styles/base.layer.css'
-import '@aws-amplify/ui-react/styles/button.layer.css'
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app'
