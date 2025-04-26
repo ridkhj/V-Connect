e21e4a0908d555e5fec76a943de646f975034fc5
devedsonalves <devedsonalves@gmail.com>
2025-04-26 11:53:41 -0300

    chore: add and config axios

--- client/src/ui/app.tsx
+++ client/src/ui/app.tsx
@@ -1,7 +1,26 @@
 import Sidebar from "@/components/sidebar"
 import Options from "@/components/options"
+import { useEffect } from "react"
+import { api } from "./constants/api"
 
 export default function App() {
+  useEffect(() => {
+    async function sendRequest() {
+      try {
+        const res = await api.post("/send-email", {
+          recipients: "devedsonalves@gmail.com",
+          subject: "V Connect",
+          body: "Olá! Este é um e-mail enviado do frontend para test"
+        });
+        console.log(res.data);
+      } catch (err) {
+        console.log('Deu erro: ' + err);
+      }
+    }
+
+    sendRequest()
+}, [])
+
   return (
     <div className="w-screen h-screen flex overflow-hidden">
       <Sidebar />
