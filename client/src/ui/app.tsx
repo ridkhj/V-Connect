e26531a2ad676d8ec579c7e090d140424a143ee5
devedsonalves <devedsonalves@gmail.com>
2025-04-28 09:17:02 -0300

    refactor: cleaning code

--- client/src/ui/app.tsx
+++ client/src/ui/app.tsx
@@ -1,16 +1,16 @@
-import Sidebar from "@/components/sidebar"
-import Options from "@/components/options"
-import { useEffect } from "react"
-import { api } from "./constants/api"
+import { useEffect } from 'react'
+import { api } from '@/constants/api'
+import Sidebar from '@/components/sidebar'
+import Options from '@/components/options'
 
 export default function App() {
   useEffect(() => {
     async function sendRequest() {
       try {
-        const res = await api.post("/send-email", {
-          recipients: "devedsonalves@gmail.com",
-          subject: "V Connect",
-          body: "Olá! Este é um e-mail enviado do frontend para test"
+        const res = await api.post('/send-email', {
+          recipients: 'devedsonalves@gmail.com',
+          subject: 'V Connect',
+          body: 'Olá! Este é um e-mail enviado do frontend para test'
         });
         console.log(res.data);
       } catch (err) {
@@ -19,12 +19,12 @@ export default function App() {
     }
 
     sendRequest()
-}, [])
+  }, [])
 
   return (
-    <div className="w-screen h-screen flex overflow-hidden">
+    <div className='w-screen h-screen flex overflow-hidden'>
       <Sidebar />
-      <div className="w-full bg-zinc-50">
+      <div className='w-full bg-zinc-50'>
         <Options />
       </div>
     </div>
